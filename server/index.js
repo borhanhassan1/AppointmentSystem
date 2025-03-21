4333c696d55fe35f417fe041e1545a1a0010c4f2
borhanhassan1 <166884594+borhanhassan1@users.noreply.github.com>
2025-03-21 10:43:19 +0200

    Update URL

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ connectDB();
 
 app.use(
   cors({
-   "https://appointment-system-chvt.vercel.app" || "http://localhost:3000",
+   origin:["https://appointment-system-chvt.vercel.app/"],
     credentials: true,
   })
 );
