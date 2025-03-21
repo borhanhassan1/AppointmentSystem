c7c7778806f9aacedb0af093de84ad37cf0805a8
borhanhassan1 <166884594+borhanhassan1@users.noreply.github.com>
2025-03-21 10:30:22 +0200

    Update URL

--- client/src/api/appointmentApi.js
+++ client/src/api/appointmentApi.js
@@ -1,13 +1,13 @@
 import http from "./http";
 
 const createAppointment = (data) =>
-  http.post(`${process.env.REACT_APP_BASE_URL}/appointment/create`, data);
+  http.post(`https://appointment-system-fawn.vercel.app/api/appointment/create`, data);
 const bookedSlots = (date) =>
-  http.get(`${process.env.REACT_APP_BASE_URL}/appointment/booked-slots`, {
+  http.get(`https://appointment-system-fawn.vercel.app/api/appointment/booked-slots`, {
     params: { date: encodeURIComponent(date) },
   });
 const getUserAppointments = (userId) =>
-  http.get(`${process.env.REACT_APP_BASE_URL}/appointment/${userId}`);
+  http.get(`https://appointment-system-fawn.vercel.app/api/appointment/${userId}`);
 
 const appointmentApi = { createAppointment, bookedSlots, getUserAppointments };
 export default appointmentApi;
